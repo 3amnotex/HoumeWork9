d8b56290f36d26c04403a2836e5946ea501eaf04
Yuri <yromanov@yandex.ru>
2023-07-10 20:52:31 +0300

    Houmework2.2

--- C#/HoumeWork1/Program.cs
+++ C#/HoumeWork1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, которая принимает на вход 
+﻿// Task 1. Напишите программу, которая принимает на вход 
 // трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 /*
@@ -14,5 +14,24 @@ int newNumber = CutNumber(randNumber);
 Console.WriteLine($"New version of {randNumber} is {newNumber}");
 */
 
-// Напишите программу, которая выводит третью цифру заданного числа
+// Task 2. Напишите программу, которая выводит третью цифру заданного числа
+
+int thirdDigit(int number)
+{
+    int newNumber = number % 100;
+    int result = newNumber;
+    return result; 
+}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) Console.WriteLine(number + "-" + "Некорректное число");
+while (number >= 1000) 
+{
+      newNumber = number / 10;
+    else newNumber = newThirdDigit(hirdDigit);
+}
+
+
+// Task 3. Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
 
