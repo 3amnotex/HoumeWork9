b5b4094b098a6113ef89cbff4a514b847a4bbc1c
Yuri <yromanov@yandex.ru>
2023-07-14 22:02:15 +0300

    First Task Seminar 4

---
+++ C#/HoumeWork4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int degreeConversion(int number, int degree)
+    {
+        int result = 1;
+
+        for (int i = 0; i < degree; i++)
+        {
+            result *= number;
+        }
+
+        return result;
+    }
+        int A = new Random().Next(1, 10);
+        Console.WriteLine(A);
+        //  Console.WriteLine("Введите число A:");
+        // int A = Convert.ToInt32(Console.ReadLine());
+        int B = new Random().Next(1, 10);
+        Console.WriteLine(B);
+        // Console.WriteLine("Введите число B:");
+        // int B = Convert.ToInt32(Console.ReadLine());
+
+        int result = degreeConversion(A, B);
+        Console.WriteLine("Результат: " + result);
+
