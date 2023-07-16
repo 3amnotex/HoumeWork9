5bf7d3c25fd00d92e7ecacacb90a5b433ef3e033
Yuri <yromanov@yandex.ru>
2023-07-16 17:28:26 +0300

    Задачи 4-го семинара

--- C#/HoumeWork4/Program.cs
+++ C#/HoumeWork4/Program.cs
@@ -1,5 +1,6 @@
-﻿// Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-int degreeConversion(int number, int degree)
+﻿// Task 1. Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+        int degreeConversion(int number, int degree)
     {
         int result = 1;
 
@@ -21,4 +22,58 @@ int degreeConversion(int number, int degree)
 
         int result = degreeConversion(A, B);
         Console.WriteLine("Результат: " + result);
+
+// Task 2. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigit(int number)
+{   
+    int sum = 0;
+    while(number > 0)
+    {       
+       sum = sum + number % 10;
+      
+       number = number / 10;
+    }
+       return sum;
+}
+Console.WriteLine("Input number: ");
+int dig = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigit(dig));
+
+// Task 3. Напишите программу, которая задаёт массив из m элементов и выводит их на экран. 
+// Разработать метод CreateArray(int size), генерирующий массив на основе данных, вводимых пользователем.
+
+ 
+    int[] CreateArray(int size)
+    {
+        int[] array = new int[size];
+        Console.WriteLine("Введите элементы массива:");
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return array;
+    }
+    
+     void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+        Console.WriteLine("Введите размер массива:");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = CreateArray(size);
+
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(array);
+        
+    
+
+
 
