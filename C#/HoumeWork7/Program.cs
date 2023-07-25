228640036ba0f8c7081799afac90c0c52ca6e788
Yuri <yromanov@yandex.ru>
2023-07-26 00:03:32 +0300

    Solution Task 2

--- C#/HoumeWork7/Program.cs
+++ C#/HoumeWork7/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Task 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
+/*
 double[,] CreateDoubleArray (int rows, int columns)
 {
    double[,] array = new double[rows, columns];
@@ -39,3 +40,73 @@ int columns = Convert.ToInt32 (Console.ReadLine());
 
 double[,] myArray = CreateDoubleArray(rows, columns);
 PrintArray(myArray);
+*/
+
+// Напишите программу, которая на вход принимает 
+// позиции элемента в двумерном массиве, и возвращает
+// значение этого элемента или же указание, что такого элемента нет
+
+int[,]  CreateMyArray(int rows, int columns, int minValue, int maxValue)
+{
+   int[,] array = new int[rows, columns];
+
+   for (int i = 0; i < rows; i++)
+   {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue +1);
+        }
+   }        
+        return array;
+
+}     
+
+void PrintArray (int[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write (array[i,j] + " ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+void PrintElementIndex(int[,] array)
+
+{
+    Console.WriteLine("Введите индекс числа строки: ");
+    int indexrows = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите индекс числа столбца: ");
+    int indexcolumns = Convert.ToInt32(Console.ReadLine());
+
+    if ( indexrows > array.GetLength(0) ||  indexcolumns > array.GetLength(1))
+    {
+        Console.WriteLine("Такого элемента в массиве нет.");
+        return;
+    }
+
+    Console.WriteLine(array[indexrows, indexcolumns]);
+}
+
+Console.WriteLine ("Input a number of rows: ");
+int rows = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Input a number of columns: ");
+int columns = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Input a number of minValue: ");
+int minValue = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Input a number of maxValue: ");
+int maxValue = Convert.ToInt32 (Console.ReadLine());
+
+
+int[,] myArray = CreateMyArray  (rows, columns, minValue, maxValue);
+PrintArray(myArray);
+
+PrintElementIndex(myArray);
+
