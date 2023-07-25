ad0ca81d88d52fa1cf4dfc19cd72973ba8295b43
Yuri <yromanov@yandex.ru>
2023-07-25 22:57:37 +0300

    Solution Task 1

---
+++ C#/HoumeWork7/Program.cs
@@ -0,0 +1,41 @@
+﻿// Task 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateDoubleArray (int rows, int columns)
+{
+   double[,] array = new double[rows, columns];
+
+    Random random = new Random();
+   
+   for (int i = 0; i < rows; i++)
+   {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = random.NextDouble() * 10 ;
+            array[i,j] = Math.Round(array[i,j], 2);
+
+        }
+   }        
+    return array;
+}         
+
+void PrintArray (double[,] array)
+
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write (array[i,j] + " ");
+        }
+        Console.WriteLine ();
+    }
+}
+
+Console.WriteLine ("Input a number of rows: ");
+int rows = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Input a number of columns: ");
+int columns = Convert.ToInt32 (Console.ReadLine());
+
+double[,] myArray = CreateDoubleArray(rows, columns);
+PrintArray(myArray);
