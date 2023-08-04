f0fc3c3cc4d027d70f5654b92655b2fdc32e75bb
Yuri <yromanov@yandex.ru>
2023-08-04 07:25:46 +0300

    solution task 3

--- C#/HoumeWork9/Program.cs
+++ C#/HoumeWork9/Program.cs
@@ -11,7 +11,7 @@ NaturalNumbers(8);
 */
 // Task 2.  Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
-
+/*
 int SumNaturalDigit (int m, int n)
 {
     if (m == n) return m;
@@ -21,3 +21,19 @@ int SumNaturalDigit (int m, int n)
 }
 
 Console.WriteLine (SumNaturalDigit(4,3));
+*/
+
+// Task 3. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman (int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}
+
+Console.WriteLine(Akkerman(2,3));
